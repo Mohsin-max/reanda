4b5769db6db7c116a95bfa3ba36c6d4b2d072a92
mohsin ali <javascript492@gmail.com>
2025-05-12 18:30:36 +0500

    logo animation added

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -2,9 +2,9 @@ import { isPlatformBrowser } from '@angular/common';
 import { AfterViewInit, Component, Inject, PLATFORM_ID } from '@angular/core';
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+
 @Component({
   selector: 'app-about',
-  imports: [],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
@@ -14,7 +14,6 @@ export class AboutComponent implements AfterViewInit {
 
   ngAfterViewInit(): void {
     if (isPlatformBrowser(this.platformId)) {
-      // Thoda delay de rahe hain ke elements load ho jayein
       setTimeout(() => {
         gsap.registerPlugin(ScrollTrigger);
 
@@ -23,14 +22,17 @@ export class AboutComponent implements AfterViewInit {
             trigger: '.mountainImg',
             start: 'top 60%',
             end: 'top 0%',
-            scrub: 2
-            
+            scrub: 2,
+            markers: false, // Debug markers (remove in production)
+            toggleActions: 'play reverse play reverse'
           }
         });
 
-        tl.to('.mountainImg', { scale: 1 });
-        tl.to('.mountainText', { opacity: 1 });
-      }, 100); // 100ms ka chota delay
+        // Animation step by step
+        tl.to('.mountainImg', { scale: 1, duration: 1 });
+        tl.to('.mountainText', { opacity: 1, duration: 1 }, '-=0.5'); // Overlap thoda
+
+      }, 100);
     }
   }
 
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,16 +1,16 @@
 import { CommonModule, isPlatformBrowser } from '@angular/common';
-import { Component, ElementRef, HostListener, Inject, PLATFORM_ID, ViewChild } from '@angular/core';
+import { AfterViewInit, OnDestroy, Component, Inject, PLATFORM_ID, HostListener } from '@angular/core';
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-
 @Component({
   selector: 'app-navbar',
+  standalone: true,
   imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent{
+export class NavbarComponent implements AfterViewInit, OnDestroy {
 
   menuOpen = false;
   navBg: any;
@@ -19,49 +19,41 @@ export class NavbarComponent{
     this.menuOpen = !this.menuOpen;
   }
 
-  @HostListener("document:scroll") Scrollover() {
+  // @HostListener("document:scroll") Scrollover() {
+  //   if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+  //     this.navBg = { 'background-color': '#003574' }
+  //   } else {
+  //     this.navBg = {}
+  //   }
+  // }
 
-    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
-
-      this.navBg = {
-
-        'background-color': '#003574'
-
-      }
-    } else {
-
-      this.navBg = {
-
-      }
-
-    }
-
-  }
-
-    constructor(@Inject(PLATFORM_ID) private platformId: Object) { }
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) { }
 
   ngAfterViewInit(): void {
     if (isPlatformBrowser(this.platformId)) {
       gsap.registerPlugin(ScrollTrigger);
 
       gsap.to('.heroLogo', {
-
-        scale: '0.5',
+        scale: 0.4,
+        y: 0,
+        x: -300,
         duration: 1,
+        ease: "power2.out",
         scrollTrigger: {
-
           trigger: '.heroLogo',
-          start: 'top 20%',
-          end: 'top 0%',
-          scrub: 2,
-
+          start: 'top top+=100',
+          end: 'bottom top+=150',
+          scrub: 1,
+          // markers: false
         }
-
-      })
+      });
 
     }
   }
-
-
+  ngOnDestroy(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      ScrollTrigger.getAll().forEach(trigger => trigger.kill());  // Corrected cleanup
+    }
+  }
 
 }
