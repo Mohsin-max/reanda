0a1697544eec5236bf84ae38a0019cd2f2f71014
mohsin ali <javascript492@gmail.com>
2025-05-13 15:02:53 +0500

    logo animation done

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule, isPlatformBrowser } from '@angular/common';
 import { AfterViewInit, OnDestroy, Component, Inject, PLATFORM_ID, HostListener } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
@@ -19,37 +20,62 @@ export class NavbarComponent implements AfterViewInit, OnDestroy {
     this.menuOpen = !this.menuOpen;
   }
 
-  // @HostListener("document:scroll") Scrollover() {
-  //   if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
-  //     this.navBg = { 'background-color': '#003574' }
-  //   } else {
-  //     this.navBg = {}
-  //   }
-  // }
+  @HostListener("document:scroll") Scrollover() {
+    if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+      this.navBg = { 'background-color': '#003574' }
+    } else {
+      this.navBg = {}
+    }
+  }
 
   constructor(@Inject(PLATFORM_ID) private platformId: Object) { }
 
   ngAfterViewInit(): void {
     if (isPlatformBrowser(this.platformId)) {
       gsap.registerPlugin(ScrollTrigger);
+      ScrollTrigger.matchMedia({
+        "(min-width: 769px)": function () {
+          // Create timeline for smooth transitions
+          const tl = gsap.timeline({
+            scrollTrigger: {
+              trigger: '.logoWrapper',
+              start: 'top 150',
+              end: 'bottom top',
+              scrub: 1,
+              toggleActions: 'play none none none',
+            }
+          });
 
-      gsap.to('.heroLogo', {
-        scale: 0.4,
-        y: 0,
-        x: -300,
-        duration: 1,
-        ease: "power2.out",
-        scrollTrigger: {
-          trigger: '.heroLogo',
-          start: 'top top+=100',
-          end: 'bottom top+=150',
-          scrub: 1,
-          // markers: false
+          // First scroll
+          tl.to('.heroLogo', {
+            scale: 0.6,
+            x: '-15%',
+            y: '-10%',
+            duration: 0.33
+          });
+
+          // Further scroll
+          tl.to('.heroLogo', {
+            scale: 0.3,
+            x: '-35%',
+            y: '-30%',
+            duration: 0.67
+          });
+
+          // Pinning the logo container
+          ScrollTrigger.create({
+            trigger: '.logoWrapper',
+            start: 'top top',
+            end: 'bottom+=8000 top',
+            pin: '.logoInner',
+            pinSpacing: false,
+          });
         }
       });
-
     }
   }
+
+
   ngOnDestroy(): void {
     if (isPlatformBrowser(this.platformId)) {
       ScrollTrigger.getAll().forEach(trigger => trigger.kill());  // Corrected cleanup
